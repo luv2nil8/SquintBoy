a0a375bd0d2a24685f7c589c1911e8eb009dbf97
Hunter <luv2nil8@gmail.com>
2026-06-26 17:19:21 -0600

    Bump Monetize to 1.3.1 prod (mobile 131, wear 231)

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.anaglych.squintboyadvance"
         minSdk = 30
         targetSdk = 36
-        versionCode = 230
-        versionName = "1.3.0"
+        versionCode = 231
+        versionName = "1.3.1"
     }
 
     signingConfigs {
