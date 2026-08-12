5b643bd4606fca501651f954256f7192e86a59da
Hunter <luv2nil8@gmail.com>
2026-08-12 16:29:17 -0600

    Revert "Bump mobile to 137 for the Pro-override gate fix"
    This reverts commit 73206f8e2b3275360e3facdd08b9a3f81e9aaa2f.

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "com.anaglych.squintboyadvance"
         minSdk = 30
         targetSdk = 36
-        versionCode = 137
+        versionCode = 136
         versionName = "1.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
